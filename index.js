c7ace80daaa4a30b251e469fc0265f9815596f87
Zach <zachary.david.hamilton@gmail.com>
2020-05-29 16:20:45 -0400

    commit message

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var app = express();
 
 // respond with "hello world" for requests that hit root "/"
 app.get('/', (req, res) => {
-    res.send('new commit');
+    res.send('another new commit');
 });
 
 // listen to port 3000 by default
