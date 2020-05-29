b2e27cda23dc841fb7fad19fce443a1a158426e1
Zach <zachary.david.hamilton@gmail.com>
2020-05-29 16:28:50 -0400

    conor commit

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var app = express();
 
 // respond with "hello world" for requests that hit root "/"
 app.get('/', (req, res) => {
-    res.send('Hello world');
+    res.send('Hello conor');
 });
 
 // listen to port 3000 by default
